32308094b2bffa43c204a55ff3773ab8a7b78d6f
Neo Alienson <neo@padblish.com>
2019-01-08 22:43:14 +0800

    remove unused code

--- arduino_boot_menu_usb_keyboard/arduino_boot_menu_usb_keyboard.ino
+++ arduino_boot_menu_usb_keyboard/arduino_boot_menu_usb_keyboard.ino
@@ -1,18 +1,12 @@
 #include <Keyboard.h>
 
-
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-
 void setup() {
-  // initialize the pushbutton pin as an input:
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
 
   Keyboard.begin();
-//  Serial.begin(9600);
 }
 
 void loop() {
